c58ab4c9ecb3150c90a2ff8f52c0f1325f466419
Robbie VanDerzee <robert.scott.vanderzee@gmail.com>
2021-07-14 17:35:05 -0400

    Preliminary changes to Event Dispatching (#1)

--- andromeda/test/core/system/event/type/window.cc
+++ andromeda/test/core/system/event/type/window.cc
@@ -20,7 +20,7 @@ TEST(Event, Resize) {
 
 TEST(Event, Close) {
     EXPECT_NO_THROW({
-        auto close = Andromeda::System::Event::Window::Close{};
+        Andromeda::System::Event::Window::Close close;
         EXPECT_EQ(close.status, Andromeda::System::Structure::Status::Event::Unused);
         close.status = Andromeda::System::Structure::Status::Event::Used;
     });
@@ -28,7 +28,7 @@ TEST(Event, Close) {
 
 TEST(Event, Refresh) {
     EXPECT_NO_THROW({
-        auto refresh = Andromeda::System::Event::Window::Refresh{};
+        Andromeda::System::Event::Window::Refresh refresh;
         EXPECT_EQ(refresh.status, Andromeda::System::Structure::Status::Event::Unused);
         refresh.status = Andromeda::System::Structure::Status::Event::Used;
     });
@@ -36,7 +36,7 @@ TEST(Event, Refresh) {
 
 TEST(Event, Focus) {
     EXPECT_NO_THROW({
-        auto focus = Andromeda::System::Event::Window::Focus{};
+        Andromeda::System::Event::Window::Focus focus;
         EXPECT_EQ(focus.status, Andromeda::System::Structure::Status::Event::Unused);
         focus.status = Andromeda::System::Structure::Status::Event::Used;
     });
@@ -44,7 +44,7 @@ TEST(Event, Focus) {
 
 TEST(Event, Defocus) {
     EXPECT_NO_THROW({
-        auto defocus = Andromeda::System::Event::Window::Defocus{};
+        Andromeda::System::Event::Window::Defocus defocus;
         EXPECT_EQ(defocus.status, Andromeda::System::Structure::Status::Event::Unused);
         defocus.status = Andromeda::System::Structure::Status::Event::Used;
     });
@@ -52,7 +52,7 @@ TEST(Event, Defocus) {
 
 TEST(Event, Maximize) {
     EXPECT_NO_THROW({
-        auto maximize = Andromeda::System::Event::Window::Maximize{};
+        Andromeda::System::Event::Window::Maximize maximize;
         EXPECT_EQ(maximize.status, Andromeda::System::Structure::Status::Event::Unused);
         maximize.status = Andromeda::System::Structure::Status::Event::Used;
     });
@@ -60,7 +60,7 @@ TEST(Event, Maximize) {
 
 TEST(Event, Minimize) {
     EXPECT_NO_THROW({
-        auto minimize = Andromeda::System::Event::Window::Minimize{};
+        Andromeda::System::Event::Window::Minimize minimize;
         EXPECT_EQ(minimize.status, Andromeda::System::Structure::Status::Event::Unused);
         minimize.status = Andromeda::System::Structure::Status::Event::Used;
     });
@@ -68,8 +68,25 @@ TEST(Event, Minimize) {
 
 TEST(Event, Restore) {
     EXPECT_NO_THROW({
-        auto restore = Andromeda::System::Event::Window::Restore{};
+        Andromeda::System::Event::Window::Restore restore;
         EXPECT_EQ(restore.status, Andromeda::System::Structure::Status::Event::Unused);
         restore.status = Andromeda::System::Structure::Status::Event::Used;
     });
+}
+
+
+TEST(Event, Callback) {
+    auto callback = [](Andromeda::System::Event::Window::Close & event) {
+        EXPECT_TRUE(event.status == Andromeda::System::Structure::Status::Event::Unused);
+        event.status = Andromeda::System::Structure::Status::Event::Used;
+    };
+    Andromeda::System::Event::Window::Close::Manager manager;
+    manager.listen(callback);
+    manager.latest();
+    Andromeda::System::Event::Window::Close event;
+    manager.queue(event);
+    manager.deafen(callback);
+    manager.latest();
+    manager.listen(callback);
+    manager.latest();
 }
