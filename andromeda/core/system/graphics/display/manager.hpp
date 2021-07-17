b90074a973c68c114e3c46d17ac19f2ecb4a717e
Robbie VanDerzee <robert.scott.vanderzee@gmail.com>
2021-07-16 20:40:26 -0400

    Improved Input state API (#1):      - Graph of build dependencies

--- andromeda/core/system/graphics/display/manager.hpp
+++ andromeda/core/system/graphics/display/manager.hpp
@@ -20,6 +20,11 @@ namespace Andromeda::System::Graphics::Display {
 
         virtual void update() = 0;
 
+        virtual bool pressed(Andromeda::System::Input::Code::Keyboard::Key key) const = 0;
+        virtual bool pressed(Andromeda::System::Input::Code::Keyboard::Mod mod) const = 0;
+        virtual bool pressed(Andromeda::System::Input::Code::Mouse::Button button) const = 0;
+        virtual Andromeda::System::Structure::Duo<double> mouse() const = 0;
+
         virtual void create(Window::Configuration configuration) = 0;
 
         static std::unique_ptr<Manager> instantiate(Manager::Configuration configuration);
